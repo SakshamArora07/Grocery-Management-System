ce8f792df908443e0c88e77d8cbde53ce1d88d50
Saksham Arora <140829150+SakshamArora07@users.noreply.github.com>
2024-08-26 14:48:35 +0530

    Update index.php

--- grocery/index.php
+++ grocery/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Welcome to Grocery2</title>
+    <title>Welcome to SMART</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 </head>
